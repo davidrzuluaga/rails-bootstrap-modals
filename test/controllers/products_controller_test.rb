689a04a2bd7b8b363c10e447bdbb43d469273ee3
Juan Camilo Hernández <camilo9020@gmail.com>
2018-09-10 17:03:56 -0500

    products controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ProductControllerTest < ActionDispatch::IntegrationTest
+class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get product_index_url
+    get products_index_url
     assert_response :success
   end
 
